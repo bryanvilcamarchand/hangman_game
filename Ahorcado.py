ff8245b07f72fdfa381e9b54730cef3b3ef27cce
bryanvimarpe <bryan.vm@icloud.com>
2022-01-22 14:20:10 -0500

    Codigo con comentarios

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,8 +1,12 @@
+# Import libreries that use in the project directory (ENG)
+# Importo las librerias que usaremos en el proyecto (SPA)
+
 import random
 import os
 import time
 
-
+# Search in google and copy hangman images to use in our project. (ENG)
+# Buscamos en google la imagen del ahorcado y la copiamos para usarlo en el proyecto. (SPA)
 
 def run():
 
@@ -59,12 +63,23 @@ def run():
 
     images2 = images[::-1]
 
+# Select the words to use in our project (ENG)
+# Elegimos las palabras que usaremos en el proyecto (SPA)
+
+
     DB = ['BANDERA', 'HIMNO', 'ESCUDO', 'ESCARAPELA']
+
+# Use 'random.choise' to randomly select a worf of our small dataset DB (ENG)
+
+# Select the word to use in the game, and show the numbers of characters of the word selected. We have 6 attemps to the game. (ENG) 
+# Seleccionamos una palabra de manera aleatoria y mostrar la cantidad de guiones de acuerdo a ella. Tenemos 6 intentos en el juego. (SPA)
 
     word = random.choice(DB)
     spaces = ['_'] * len(word)
     attempts = 6
 
+# Write the message to use in the intro of the game. We use time.sleep to appers dots to expect another messages. (ENG)
+# Escribimos el mensaje que usaremos en el intro del juego. Usamos time.sleep para que aparezcan puntos mientras esperamos que aparezcan todos los mensajes. (SPA)
   
     print(''' 
            Bivenvenido a la Trivia de símbolos patrios.
@@ -110,6 +125,9 @@ def run():
     time.sleep(2)
 
 
+# Create an infinitive cicle, and shows the numbers of characters of the selected word. Then, ask the user for a letter. (ENG)
+# Creamos un ciclo infinito, mostramos el numero de caracteres de la palabra seleccionada. Luego, le pedimos al usuario ingrese una letra. (SPA)
+
     while True:
         os.system('clear')
         for character in spaces:
@@ -117,11 +135,15 @@ def run():
         print(images2[attempts])
         letter = input('Elige una letra: ').upper()
 
+# Create the logical rules to guess the word. If you don't guess the word, you lose an attemp. If the number of attemps is 0, you lose the game. (ENG)
+# Creamos la lógica para adivinar una letra de la palabra seleccionada. Si no aciertas, pierdes intentos. Si la cantidad de intentos es cero, pierdes el juego. (SPA)
+
         found = False
         for idx, character in enumerate(word):
             if character == letter:
                 spaces[idx] = letter
                 found = True
+
 
         if not found:
             attempts -= 1
@@ -137,8 +159,6 @@ def run():
             print('Perdiste')
             break
             input()
-
-
 
 
 if __name__ == '__main__':
